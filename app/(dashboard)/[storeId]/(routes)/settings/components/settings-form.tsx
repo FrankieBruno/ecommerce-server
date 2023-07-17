f02f04ec32d4265013f20b9424b6b88fc8f95ca8
Frankie Bruno <frankiebruno1991@gmail.com>
2023-07-17 11:31:27 -0500

    july17th

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -87,7 +87,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
       onConfirm={onDelete}
       loading={loading}
     />
-     <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between">
         <Heading title="Store settings" description="Manage store preferences" />
         <Button
           disabled={loading}
@@ -95,12 +95,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
           size="sm"
           onClick={() => setOpen(true)}
         >
-          <Trash className="h-4 w-4" />
+          <Trash className="w-4 h-4" />
         </Button>
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
